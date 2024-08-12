c04ab05593219734dba3f5c40c7cf701af7b30ee
Eduard Valiyllin <eduardworrk@gmail.com>
2024-08-12 14:46:26 +0300

    убрал не правильный import

--- src/components/delete-modal/delete-modal.test.tsx
+++ src/components/delete-modal/delete-modal.test.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { DeleteModal } from ".";
-import renderer from 'react-test-renderer';
 
 describe("DeleteModal Component", () => {
   const props = {
